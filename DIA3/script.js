6f546bb3829cedb89535ee21824377a26d08baa5
Alesandro Alves <alessandroasf5@gmail.com>
2023-09-16 15:27:05 -0300

    ajustando erro

---
+++ DIA3/script.js
@@ -0,0 +1,13 @@
+// let e const
+
+let meuNome = "Alessandro Alves"
+console.log(meuNome)
+meuNome = "Giovanni Sacchitiello"
+console.log(meuNome)
+// o valor da variável se altera com o LET
+console.log("============= constante =============")
+// o valor da varivale não se altera com o const
+const a = 2
+console.log(a)
+a = 5
+console.log(a)
