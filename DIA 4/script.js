b6024eea89e1625de1c603dc04349406f907c774
Alesandro Alves <alessandroasf5@gmail.com>
2023-09-17 13:20:48 -0300

    escopos

--- DIA 4/script.js
+++ DIA 4/script.js
@@ -1,5 +1,5 @@
 console.log("=============");
-console.log("CONDICIONAIS");
+console.log("ESCOPO");
 console.log("=============");
 
 let idade = Number(prompt("Insira sua idade"))
@@ -7,10 +7,17 @@ let idade = Number(prompt("Insira sua idade"))
 // === igual ao valor e o tipo da variável | !== é diferente o valor e o tipo da variável 
 
 if (idade <= 10) {
+    console.log(idade);
+    let dentroEscopo = "Giovanni"
+    console.log(dentroEscopo);
     console.log("Voce tem menos ou 10 anos");
+    if (true) {
+    console.log("estou no terceiro escopo");
+    console.log(dentroEscopo);        
+    }
 } else {
     console.log("Voce tem mais que 10 anos");
 }
 
-
+console.log("SAI DO ESCOPO  DO IF");
 
