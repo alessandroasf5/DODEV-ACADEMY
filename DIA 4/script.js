f85ecb13d1b21c55850eec0e89c397cfe0473841
Alesandro Alves <alessandroasf5@gmail.com>
2023-09-17 13:14:42 -0300

    condicionais

---
+++ DIA 4/script.js
@@ -0,0 +1,16 @@
+console.log("=============");
+console.log("CONDICIONAIS");
+console.log("=============");
+
+let idade = Number(prompt("Insira sua idade"))
+// == igual | != diferente | < menor que | > maior que | <= menor ou igual | >= maior ou igual 
+// === igual ao valor e o tipo da variável | !== é diferente o valor e o tipo da variável 
+
+if (idade <= 10) {
+    console.log("Voce tem menos ou 10 anos");
+} else {
+    console.log("Voce tem mais que 10 anos");
+}
+
+
+
